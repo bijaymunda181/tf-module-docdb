57c1b3bbee2a563a7673e35629696336662a4b11
Bijay <bijay78082@gmail.com>
2026-08-29 12:31:46 +0530

    terraform

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_docdb_subnet_group" "main" {
   name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.subnet_ids
 
-  tags = merge(local.tags, { "${local.name_prefix}-subnet-group" })
+  tags = merge(local.tags, { name = "${local.name_prefix}-subnet-group" })
 }
